4b180f951264297aeba93c5016c7123d87303ede
pauloricardodn <32545045+pauloricardodn@users.noreply.github.com>
2021-10-07 00:32:34 -0300

    Exemplo utilizado na aplesentação

---
+++ EverisStore/EverisSore.Domain/EntidadeBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EverisSore.Domain
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; set; }
+        public DateTime DataCastrastro { get; set; } = DateTime.Now;
+        protected bool Ativo { get; set; }
+    }
+}
